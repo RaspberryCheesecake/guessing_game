a6ef2ed89d25e3f6ad1ca915fc48e5a097dda6ad
Hannah Hazi <hannah.hazi@cantab.net>
2017-12-31 23:38:22 +0000

    Added loop and break condition. But I don't like hard-coding the 'You win' everwhere like this!

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,28 @@ fn random_comparison_win(guess: u32, secret: u32) -> std::string::String {
 
 fn main() {
 	println!("Guess the number between 1 and 100!");
-	println!("Please input your guess:");
-    
-	let mut guess = String::new();  //Make an empty string to hold guesses
-	io::stdin().read_line(&mut guess)
-		.expect("Oh no! Failed to read input!");  
-	// populate it with whatever the user types
-    
-	let guess_int: u32 = guess.trim().parse()
-		.expect("Please type an integer number only!");
-	// coerce to unsigned 32 bit integer, if you can
-
-	let secret_number = rand::thread_rng().gen_range(1, 101);
 	
-	let result = random_comparison_win(guess_int, secret_number);
-	println!("{}", result);
+	loop {
+		println!("Please input your guess:");
+
+		let mut guess = String::new();  //Make an empty string to hold guesses
+		io::stdin().read_line(&mut guess)
+			.expect("Oh no! Failed to read input!");  
+		// populate it with whatever the user types
+	    
+		let guess_int: u32 = guess.trim().parse()
+			.expect("Please type an integer number only!");
+		// coerce to unsigned 32 bit integer, if you can
+
+		let secret_number = rand::thread_rng().gen_range(1, 101);
+
+		let result = random_comparison_win(guess_int, secret_number);
+		println!("{}", result);
+
+		if result == String::from("You win!") {
+		break;		
+		}
+	}
 }
 
 #[cfg(test)]
