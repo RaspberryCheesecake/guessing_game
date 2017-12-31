48d3295b7326db32a557ef62ead09d02768950b2
Hannah Hazi <hannah.hazi@cantab.net>
2017-12-31 17:25:41 +0000

    Added failing test

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate rand;
 
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
 	let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -19,4 +20,10 @@ fn main() {
 	
 	println!("You guessed: {}", guess_int);
 	println!("The secret number is {}", secret_number);
+	
+	match guess_int.cmp(&secret_number) {
+	Ordering::Less =>	println!("Too small!"),
+	Ordering::Equal =>	println!("You win!"),
+	Ordering::Greater =>	println!("Too big!"),	
+	}
 }
