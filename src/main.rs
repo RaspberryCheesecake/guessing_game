6309e5a8ee3636b35a66c38080e47c9eb85bcde1
Hannah Hazi <hannah.hazi@cantab.net>
2018-01-01 15:24:31 +0000

    Improved gameplay, added guess counter. It's still not really an interesting game though. Also, typing 'quit' doesn't quit the game - I wonder why?

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
-const YAY: &'static str = "You win!";
+const YAY: &'static str = "You WIN!!";
 
-fn random_comparison_win(guess: u32, secret: u32) -> &'static str {
-	println!("You guessed: {}", guess);
-	println!("The secret number is {}", secret);
+pub fn random_comparison_win(guess: u32, secret: u32) -> &'static str {
+	//println!("You guessed: {}", guess);
+	//println!("The secret number is {}", secret);
 	
 	match guess.cmp(&secret) {
 	// returns an answer string based on comparison with guess
@@ -20,25 +20,32 @@ fn random_comparison_win(guess: u32, secret: u32) -> &'static str {
 
 fn main() {
 	println!("Guess the number between 1 and 100!");
+	let secret_number = rand::thread_rng().gen_range(1, 101);
+	let mut n_guesses: u32 = 0;
 	
 	loop {
 		println!("Please input your guess:");
-
+		
 		let mut guess = String::new();  //Make an empty string to hold guesses
+
 		io::stdin().read_line(&mut guess)
 			.expect("Oh no! Failed to read input!");  
 		// populate it with whatever the user types
-	    
-		let guess_int: u32 = guess.trim().parse()
-			.expect("Please type an integer number only!");
-		// coerce to unsigned 32 bit integer, if you can
 
-		let secret_number = rand::thread_rng().gen_range(1, 101);
+		if guess == String::from("quit") {break;};
+	    
+		let guess_int: u32 = match guess.trim().parse() {
+			Ok(num) => {n_guesses += 1; num},
+			Err(_) => {println!("Please type an integer number only!");
+					continue;}
+		}; 
+		// coerce to unsigned 32 bit integer, if you can
 
 		let result = random_comparison_win(guess_int, secret_number);
 		println!("{}", result);
 
 		if result == YAY {
+		println!("It took you {} guesses!", n_guesses);
 		break;		
 		}
 	}
@@ -48,15 +55,12 @@ fn main() {
 mod tests {
     #[test]
     fn test_when_numbers_same_returns_yay() {
-	use super::*;  // to allow use to use non pub fn declared in this file
-	
 	let num: u32 = rand::thread_rng().gen_range(1, 101);
 	let result = random_comparison_win(num, num);
     	assert_eq!(result, YAY);
     }
     #[test]
     fn test_when_numbers_differ_you_do_not_win() {
-	use super::*;
 	let num: u32 = rand::thread_rng().gen_range(1, 101);
 	let other_num = num + 1;
 	let result = random_comparison_win(num, other_num);
